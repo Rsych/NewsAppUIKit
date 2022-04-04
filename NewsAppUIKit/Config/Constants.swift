fdb6ff8191efea3e355c8a81105c2c8ff2765dfe
Ryan <tamielryan@gmail.com>
2022-04-04 18:03:13 +0300

    model

---
+++ NewsAppUIKit/Config/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  NewsAppUIKit
+//
+//  Created by Ryan J. W. Kim on 2022/04/04.
+//
+
+import Foundation
+
+struct APIKey {
+    static let key = ""
+}
---
+++ NewsAppUIKit/Model/News.swift
@@ -0,0 +1,16 @@
+//
+//  News.swift
+//  NewsAppUIKit
+//
+//  Created by Ryan J. W. Kim on 2022/04/04.
+//
+
+import Foundation
+
+struct News: Codable {
+    let author: String?
+    let title: String?
+    let description: String?
+    let urlToImage: String?
+    let url: String?
+}
